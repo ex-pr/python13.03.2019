f0c5280d03bbf56acb091d02736ee869551c3012
Дубовская Дарья <47379337+ex-pr@users.noreply.github.com>
2019-03-20 19:27:31 +0400

    Update 6.py

--- 6.py
+++ 6.py
@@ -3,3 +3,9 @@
 a = int(input('Введите номер буквы: ')) + 96
 
 print('Под этим номером в алфавите находится буква : ', chr(a))
+
+# import string
+#
+# letter = input("Input letter 1: ")
+#
+# print(string.ascii_lowercase.index(letter) + 1)
